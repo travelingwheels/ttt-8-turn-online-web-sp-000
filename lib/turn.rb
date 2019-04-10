ba09b1417f45dccd5136fdbe4300930082598f12
Gary Wheeler <gkwheeler40@gmail.com>
2019-04-10 11:47:42 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -34,3 +34,14 @@ def position_taken?(board, index)
    end
 end
 
+def turn (board)
+  puts "Please enter 1-9:"
+  num = gets.chomp
+  index = input_to_index(num)
+  if valid_move?(board, index) == true
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
